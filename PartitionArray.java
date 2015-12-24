d9d48655529c4bde2fcf59f23ac4872bc33a6b39
zhangxin0804 <zhan413@usc.edu>
2015-12-24 00:32:35 -0800

    Update PartitionArray.java

--- PartitionArray.java
+++ PartitionArray.java
@@ -1,33 +1,7 @@
-// Solution 4 -- two pointers, 一个维护<k元素的left boundary指针，一个是遍历指针。
-//               类似Leetcode Move Zeroes那个题的思路。
-// 时间复杂度: O(n)
-// 空间复杂度: O(1)
-public class Solution {
-    public int partitionArray(int[] nums, int k) {
-	    if( nums == null || nums.length == 0 ){
-	        return 0;
-	    }
-	    int length = nums.length;
-	    int left = 0;
-	    for(int i = 0; i < length; i++){
-	        if( nums[i] < k ){
-	            swap(nums, left, i);
-	            left++;
-	        }
-	    }
-	    return left;
-    }
-    private void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-}
+// Solution 4 -- two pointers, 一个维护<k元素的leftboundary指针，一个是遍历指针
+//               类似Leetcode Move Zeroes那个题的思路。问题转化为将原始数组
+//               partition成两个部分。
 
-// Solution 3 --- 三指针，左右两个boundary指针以及一个遍历指针，通过SWAP把元素重新place
-//                类似Leetcode Sort Colors的其中一个解法。
-//                left始终表示<k的元素将要被放置到的index位置
-//                right始终表示>=k的元素将要被放置到的index位置。
 // 时间复杂度: O(n)
 // 空间复杂度: O(1)
 
@@ -38,19 +12,59 @@ public class Solution {
 	        return 0;
 	    }
 	    int length = nums.length;
-	    int left = 0, right = length - 1;
-	    int crt = 0;
-	    while( crt <= right ){
-	        if( nums[crt] < k ){
-	            nums[left] = nums[crt];
+	    // 注意：left指针始终指向<k元素将要被放置的Index位置。
+	    int left = 0;
+	    for(int i = 0; i < length; i++){
+	        if( nums[i] < k ){
+	            swap(nums, left, i);
 	            left++;
-	            crt++;
-	        }else{
-	            swap(nums, crt, right);
-	            right--;
 	        }
 	    }
-	    return right+1;
+	    // 因为left指针的含义如上，所以最终直接返回left即为>=k的第一个index
+	    return left;
+    }
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+*/
+
+// Solution 3 -- 三指针，左右两个boundary指针以及一个遍历指针，类似
+//                Leetcode Sort Colors的其中一个解法。
+//                left始终表示<k的元素将要被放置到的index位置
+//                right始终表示>k的元素将要被放置到的index位置。
+//                中间分布=k的元素，相当于把原始数组partition成3个部分。
+
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+
+/*
+public class Solution {
+    public int partitionArray(int[] nums, int k) {
+	    if( nums == null || nums.length == 0 ){
+	        return 0;
+	    }
+	    int length = nums.length;
+	    // left指针始终指向<k元素将要被放置的index位置。
+	    // right指针始终指向>k元素将要被放置的index位置
+	    // 中间部分则为=k元素的分布。
+	    int left = 0, right = length - 1;
+        int crt = 0;
+        while( crt <= right ){
+            if( nums[crt] == k ){
+                crt++;
+            }else if( nums[crt] < k ){
+                swap(nums, crt, left);
+                left++;
+                crt++;
+            }else{
+                swap(nums, crt, right);
+                right--;
+            }
+        }
+        return left;
     }
     
     private void swap(int[] nums, int i, int j){
@@ -65,7 +79,7 @@ public class Solution {
 //                注意循环条件的边界控制！！！取等不取等要分析！！
 // 时间复杂度: O(n)
 // 空间复杂度: O(1), in-place实现。
-/*
+
 public class Solution {
 
     public int partitionArray(int[] nums, int k) {
@@ -99,10 +113,13 @@ public class Solution {
         nums[j] = temp;
     }
 }
-*/
+
 
 // Solution 1 --- 开辟额外数组进行copy,然后再partition重新place元素到原始数组。
 //                two pointers使用，从两边往中间夹逼。
+//                left代表的含义是下一个 < k的元素要被放置的位置。
+//                right代表的含义是下一个 >= k的元素要被放置的位置。
+
 // 时间复杂度: O(n)
 // 空间复杂度: O(n)
 /*
@@ -130,8 +147,7 @@ public class Solution {
                 nums[left++] = res[i];
             }
         }
-        // 别忘了+1
-        return right+1;
+        return left;
     }
 }
 */
